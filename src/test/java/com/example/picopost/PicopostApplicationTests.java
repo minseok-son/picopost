2725088529e0f773fa0d4cb3531ac9a526eacb61
mseok <mseok.son@gmail.com>
2025-10-26 20:49:28 -0700

    Initial commit of project files

---
+++ src/test/java/com/example/picopost/PicopostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picopost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicopostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
